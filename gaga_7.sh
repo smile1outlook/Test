f68740ba3da8c74eeb05e7076bf9b5ac43ad3e1e
smile1outlook <153255193+smile1outlook@users.noreply.github.com>
2023-12-14 13:23:54 +0700

    Update gaga_7.sh

--- gaga_7.sh
+++ gaga_7.sh
@@ -4,7 +4,7 @@ apt update && apt install sudo docker.io curl && sudo rm -rf /sbin/initctl && su
 
 
 # City=("United States" "Netherlands" "Japan")
-# i=0
+i=0
 # for city in "${City[@]}"; do
 #   docker run -itd --name vpn$i --cap-add=NET_ADMIN --env VPN_SERVICE_PROVIDER=protonvpn --env OPENVPN_USER=o1Qk8A7NBqkFRJms --env OPENVPN_PASSWORD=zKBloSojLyfmQtU6jex9UY4QU2YeczL0 --env SERVER_COUNTRIES="$city" --env FREE_ONLY=on -p 90$i:90$i -p 514$i:514$i -p 514$i:514$i/udp qmcgaw/gluetun && sleep 10 && sudo docker run -itd --name gaga$i --network=container:vpn$i --env TOKEN=hbzrwiekmvbdlaqudd1ea590f967ccf9 jepbura/gaganode
 #   echo $i $city
@@ -13,23 +13,23 @@ apt update && apt install sudo docker.io curl && sudo rm -rf /sbin/initctl && su
 
 # City_ipvanish=("Ashburn" "Atlanta" "Boston" "Charlotte" "Chicago" "Dallas" "Cincinnati" "Miami" "New York" "Phoenix" "San Jose" "Seattle" "Denver" "Las Vegas" "Los Angeles" "Houston" "Montreal" "Luxembourg" "Madrid" "Manchester" "Marseille" "Melbourne" "Marseille" "Paris" "Milan" "Tirana" "Sydney" "Vienna" "Brussels" "Sao Paulo" "Toronto" "Bogota" "Prague" "Budapest" "Dublin" "Guadalajara" "Oslo")
 
-#ipvanish_resvpn=("atl-a50.ipvanish.com" "lax-a07.ipvanish.com" "lax-a38.ipvanish.com" "nyc-a47.ipvanish.com" "den-a07.ipvanish.com" "lax-a38.ipvanish.com" "iad-a07.ipvanish.com" "sea-a07.ipvanish.com" "atl-a25.ipvanish.com" "chi-a12.ipvanish.com" "nyc-a07.ipvanish.com" "sjc-a07.ipvanish.com" "iad-a71.ipvanish.com" clt-c41.ipvanish.com "syd-a23.ipvanish.com" "mia-a13.ipvanish.com" lax-b28.ipvanish.com "atl-a63.ipvanish.com" "nrt-a17.ipvanish.com" "bod-c06.ipvanish.com" "beg-c04.ipvanish.com" "par-a19.ipvanish.com" "sto-a07.ipvanish.com" "mad-a07.ipvanish.com" "lin-a19.ipvanish.com")
+ipvanish_resvpn=("atl-a44.ipvanish.com" "lax-a02.ipvanish.com" "lax-a33.ipvanish.com" "nyc-a42.ipvanish.com" "den-a02.ipvanish.com" "lax-a33.ipvanish.com" "iad-a02.ipvanish.com" "sea-a02.ipvanish.com" "atl-a20.ipvanish.com" "chi-a08.ipvanish.com" "nyc-a02.ipvanish.com" "sjc-a02.ipvanish.com" "iad-a76.ipvanish.com" "chi-a02.ipvanish.com" "syd-a28.ipvanish.com" "mia-a08.ipvanish.com" "mia-a01.ipvanish.com" "atl-a68.ipvanish.com" "sel-a02.ipvanish.com" "mrs-c06.ipvanish.com" "otp-c05.ipvanish.com" "par-a24.ipvanish.com" "sto-a02.ipvanish.com" "mad-a02.ipvanish.com" "lin-a14.ipvanish.com" "atl-a45.ipvanish.com" "lax-a03.ipvanish.com" "lax-a34.ipvanish.com" "nyc-a43.ipvanish.com" "den-a03.ipvanish.com" "lax-a34.ipvanish.com" "iad-a03.ipvanish.com" "sea-a03.ipvanish.com" "atl-a21.ipvanish.com" "chi-a09.ipvanish.com" "nyc-a03.ipvanish.com" "sjc-a03.ipvanish.com" "iad-a75.ipvanish.com" "chi-a03.ipvanish.com" "syd-a27.ipvanish.com" "mia-a09.ipvanish.com" "mia-a03.ipvanish.com" "atl-a67.ipvanish.com" "sel-a01.ipvanish.com" "mrs-c05.ipvanish.com" "otp-c06.ipvanish.com" "par-a23.ipvanish.com" "sto-a03.ipvanish.com" "mad-a03.ipvanish.com" "lin-a15.ipvanish.com")
 
-#for city in "${ipvanish_resvpn[@]}"; do
-#  docker run -itd --name vpn$i --cap-add=NET_ADMIN --env VPN_SERVICE_PROVIDER=ipvanish --env OPENVPN_USER=isinift180115@gmail.com --env OPENVPN_PASSWORD=Hoarse15 --env SERVER_HOSTNAMES="$city" -p 90$i:90$i -p 514$i:514$i -p 514$i:514$i/udp qmcgaw/gluetun && sleep 30 && sudo docker run -itd --name gaga$i --network=container:vpn$i --env TOKEN=hbzrwiekmvbdlaqudd1ea590f967ccf9 jepbura/gaganode
-#  docker logs --tail 5 vpn$i
-#  echo $i $city
-#  i=$(($i+1))
-#done
+for city in "${ipvanish_resvpn[@]}"; do
+ docker run -itd --name vpn$i --cap-add=NET_ADMIN --env BLOCK_MALICIOUS=off --env BLOCK_SURVEILLANCE=off --env BLOCK_ADS=off --env DOT=off --env VPN_SERVICE_PROVIDER=ipvanish --env OPENVPN_USER=isinift180115@gmail.com --env OPENVPN_PASSWORD=Hoarse15 --env SERVER_HOSTNAMES="$city" -p 90$i:90$i -p 514$i:514$i -p 514$i:514$i/udp qmcgaw/gluetun && sleep 10 && sudo docker run -itd --name gaga$i --network=container:vpn$i --env TOKEN=hbzrwiekmvbdlaqudd1ea590f967ccf9 jepbura/gaganode
+ docker logs --tail 5 vpn$i
+ echo $i $city
+ i=$(($i+1))
+done
 
 # purevpn_city=("Miami" "Phoenix" "Los Angeles" "Chicago" "New Jersey" "New York" "Houston" "Atlanta" "Washington DC" "Ashburn" "San Francisco" "Seattle" "Salt Lake City" "Seoul" "Melbourne" "Brisbane" "Sydney" "Perth")
-purevpn_region=("Washington" "Virginia" "Utah" "Arizona" "California" "Florida" "Georgia" "Illinois" "New York" "Texas" "Washington" "Virginia" "Utah" "Arizona" "California" "Florida" "Georgia" "Illinois" "New York" "Texas" "Washington" "Virginia" "Utah" "Arizona" "California" "Florida" "Georgia" "Illinois" "Washington" "Virginia" "Utah" "Arizona" "California" "Florida" "Georgia" "Illinois" "New York" "Texas" "Washington" "Virginia" "Utah" "Arizona" "California" "Florida" "Georgia" "Illinois" "New York" "Texas" "Washington" "Virginia" "Utah" "Arizona" "California" "Florida" "Georgia" "Illinois")
-for city in "${purevpn_region[@]}"; do
-  docker run -itd --name vpn$i --cap-add=NET_ADMIN --env BLOCK_MALICIOUS=off --env BLOCK_SURVEILLANCE=off --env BLOCK_ADS=off --env DOT=off --env VPN_SERVICE_PROVIDER=purevpn --env OPENVPN_USER=purevpn0s7712364 --env OPENVPN_PASSWORD=bn0f9y3mn2qf --env SERVER_REGIONS="$city" -p 90$i:90$i -p 514$i:514$i -p 514$i:514$i/udp qmcgaw/gluetun && sleep 20 && sudo docker run -itd --name gaga$i --network=container:vpn$i --env TOKEN=hbzrwiekmvbdlaqudd1ea590f967ccf9 jepbura/gaganode
-  docker logs vpn$i
-  echo $i $city
-  i=$(($i+1))
-done
+# purevpn_region=("Washington" "Virginia" "Utah" "Arizona" "California" "Florida" "Georgia" "Illinois" "New York" "Texas" "Washington" "Virginia" "Utah" "Arizona" "California" "Florida" "Georgia" "Illinois" "New York" "Texas" "Washington" "Virginia" "Utah" "Arizona" "California" "Florida" "Georgia" "Illinois" "Washington" "Virginia" "Utah" "Arizona" "California" "Florida" "Georgia" "Illinois" "New York" "Texas" "Washington" "Virginia" "Utah" "Arizona" "California" "Florida" "Georgia" "Illinois" "New York" "Texas" "Washington" "Virginia" "Utah" "Arizona" "California" "Florida" "Georgia" "Illinois")
+# for city in "${purevpn_region[@]}"; do
+#   docker run -itd --name vpn$i --cap-add=NET_ADMIN --env BLOCK_MALICIOUS=off --env BLOCK_SURVEILLANCE=off --env BLOCK_ADS=off --env DOT=off --env VPN_SERVICE_PROVIDER=purevpn --env OPENVPN_USER=purevpn0s7712364 --env OPENVPN_PASSWORD=bn0f9y3mn2qf --env SERVER_REGIONS="$city" -p 90$i:90$i -p 514$i:514$i -p 514$i:514$i/udp qmcgaw/gluetun && sleep 20 && sudo docker run -itd --name gaga$i --network=container:vpn$i --env TOKEN=hbzrwiekmvbdlaqudd1ea590f967ccf9 jepbura/gaganode
+#   docker logs vpn$i
+#   echo $i $city
+#   i=$(($i+1))
+# done
 # export Country="United States" &&for i in {0..4}; do docker run -itd --name vpn$i --cap-add=NET_ADMIN --env VPN_SERVICE_PROVIDER=protonvpn --env OPENVPN_USER=o1Qk8A7NBqkFRJms --env OPENVPN_PASSWORD=zKBloSojLyfmQtU6jex9UY4QU2YeczL0 --env SERVER_COUNTRIES="$Country" --env FREE_ONLY=on -p 90$i:90$i -p 514$i:514$i -p 514$i:514$i/udp qmcgaw/gluetun && sleep 10 && sudo docker run -itd --name gaga$i --network=container:vpn$i --env TOKEN=hbzrwiekmvbdlaqudd1ea590f967ccf9 jepbura/gaganode; done
 #   
 while :; do echo 'Hit CTRL+C'; sleep 30; done
