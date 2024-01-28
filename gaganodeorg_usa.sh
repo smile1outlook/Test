c5d8e8895c3e98086b2275b4a98191f4b3f869e7
smile1outlook <153255193+smile1outlook@users.noreply.github.com>
2024-01-28 22:36:03 +0700

    Update gaganodeorg_usa.sh

--- gaganodeorg_usa.sh
+++ gaganodeorg_usa.sh
@@ -8,7 +8,7 @@ apt update && apt install sudo docker.io curl
 # City=("United States" "Netherlands" "Japan")
 #i=1
 # for city in "${City[@]}"; do
-#   docker run -itd --name vpn$i --cap-add=NET_ADMIN --env VPN_SERVICE_PROVIDER=protonvpn --env OPENVPN_USER=o1Qk8A7NBqkFRJms --env OPENVPN_PASSWORD=zKBloSojLyfmQtU6jex9UY4QU2YeczL0 --env SERVER_COUNTRIES="$city" --env FREE_ONLY=on -p 90$i:90$i -p 514$i:514$i -p 514$i:514$i/udp qmcgaw/gluetun && sleep 10 && sudo docker run -itd --name gaga$i --network=container:vpn$i --env TOKEN=hbzrwiekmvbdlaqudd1ea590f967ccf9 jepbura/gaganode
+#   docker run -itd --name vpn$i --cap-add=NET_ADMIN --env VPN_SERVICE_PROVIDER=protonvpn --env OPENVPN_USER=o1Qk8A7NBqkFRJms --env OPENVPN_PASSWORD=zKBloSojLyfmQtU6jex9UY4QU2YeczL0 --env SERVER_COUNTRIES="$city" --env FREE_ONLY=on -p 7$i:7$i -p 52$i:514$i -p 52$i:52$i/udp qmcgaw/gluetun && sleep 10 && sudo docker run -itd --name gaga$i --network=container:vpn$i --env TOKEN=hbzrwiekmvbdlaqudd1ea590f967ccf9 jepbura/gaganode
 #   echo $i $city
 #   i=$(($i+1))
 # done
