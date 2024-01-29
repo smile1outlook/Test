f5959c64c3942900058ca443741e0a6457b24dc5
smile1outlook <153255193+smile1outlook@users.noreply.github.com>
2024-01-30 00:27:46 +0700

    Update smilegaga.sh

--- smilegaga.sh
+++ smilegaga.sh
@@ -30,7 +30,8 @@ create_dock () {
 sleep 10
 
 while :; do for i in {1..99}; do
-  index=$(($RANDOM % $size)) && create_dock && until docker logs --tail 2 gaga$i | grep 'node started'; do if docker logs --tail 4 gaga$i | grep -E 'vpn|err:|node config will|ERRO|command not found|node exit'; then index=$(($RANDOM % $size)) && docker stop vpn$i gaga$i && sleep 5 && docker rm vpn$i gaga$i && sleep 5 && create_dock && sleep 10; else echo retrying.. $i && docker logs --tail 4 gaga$i && sleep 10; fi; done
+  ## index=$(($RANDOM % $size)) && create_dock && 
+  until docker logs --tail 2 gaga$i | grep 'node started'; do if docker logs --tail 4 gaga$i | grep -E 'vpn|err:|node config will|ERRO|command not found|node exit'; then index=$(($RANDOM % $size)) && docker stop vpn$i gaga$i && sleep 5 && docker rm vpn$i gaga$i && sleep 5 && create_dock && sleep 10; else echo retrying.. $i && docker logs --tail 4 gaga$i && sleep 10; fi; done
   docker logs --tail 3 gaga$i
   echo $i
 done && sleep 100; done
