946abf9215703a16e1cfe4411ae16611c9ebdbd6
smile1outlook <153255193+smile1outlook@users.noreply.github.com>
2024-02-09 00:50:44 +0700

    Update testss.sh

--- testss.sh
+++ testss.sh
@@ -7,9 +7,9 @@ docker pull qmcgaw/gluetun
 docker pull jepbura/gaganode
 
 for i in {1..100}; do
-  docker run -itd --rm --cap-add=NET_ADMIN --name vpn$i -e BLOCK_MALICIOUS=off -e BLOCK_SURVEILLANCE=off -e BLOCK_ADS=off -e DOT=off -e VPN_SERVICE_PROVIDER=windscribe -e VPN_TYPE=openvpn -e OPENVPN_USER="6sujvyhr-jpwncz4" -e OPENVPN_PASSWORD="h3dfk2wsy6" -e SERVER_REGIONS="${array[$index]}" qmcgaw/gluetun
+  docker run -itd --cap-add=NET_ADMIN --name vpn$i -e BLOCK_MALICIOUS=off -e BLOCK_SURVEILLANCE=off -e BLOCK_ADS=off -e DOT=off -e VPN_SERVICE_PROVIDER=windscribe -e VPN_TYPE=openvpn -e OPENVPN_USER="6sujvyhr-jpwncz4" -e OPENVPN_PASSWORD="h3dfk2wsy6" -e SERVER_REGIONS="${array[$index]}" qmcgaw/gluetun
   until docker top vpn$i; do echo hello && sleep 2; done
-  docker run -itd --rm --name gaga$i --network=container:vpn$i --env TOKEN=hbzrwiekmvbdlaqudd1ea590f967ccf9 jepbura/gaganode
+  docker run -itd --name gaga$i --network=container:vpn$i --env TOKEN=hbzrwiekmvbdlaqudd1ea590f967ccf9 jepbura/gaganode
   docker logs vpn$i && sleep 15;
   done
 
