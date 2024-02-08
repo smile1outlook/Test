38d75c7993763c7c9f612ac0795b6c870c26d3e2
smile1outlook <153255193+smile1outlook@users.noreply.github.com>
2024-02-09 00:51:36 +0700

    Update testss.sh

--- testss.sh
+++ testss.sh
@@ -9,8 +9,9 @@ docker pull jepbura/gaganode
 for i in {1..100}; do
   docker run -itd --cap-add=NET_ADMIN --name vpn$i -e BLOCK_MALICIOUS=off -e BLOCK_SURVEILLANCE=off -e BLOCK_ADS=off -e DOT=off -e VPN_SERVICE_PROVIDER=windscribe -e VPN_TYPE=openvpn -e OPENVPN_USER="6sujvyhr-jpwncz4" -e OPENVPN_PASSWORD="h3dfk2wsy6" -e SERVER_REGIONS="${array[$index]}" qmcgaw/gluetun
   until docker top vpn$i; do echo hello && sleep 2; done
+  sleep 20
   docker run -itd --name gaga$i --network=container:vpn$i --env TOKEN=hbzrwiekmvbdlaqudd1ea590f967ccf9 jepbura/gaganode
-  docker logs vpn$i && sleep 15;
+  docker logs vpn$i && sleep 5;
   done
 
 while :; do echo 'Telegram: @smilesense'; sleep 30; done
