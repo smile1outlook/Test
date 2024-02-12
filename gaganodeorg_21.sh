25b61782db9d633d92c47477610a7ed15392d255
smile1outlook <153255193+smile1outlook@users.noreply.github.com>
2024-02-12 20:38:23 +0700

    Create gaganodeorg_21.sh

---
+++ gaganodeorg_21.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+array=("US West" "US East" "US Central" "South Korea") 
+
+size=${#array[@]}
+
+for i in {1..100}; do
+  index=$(($RANDOM % $size))
+  docker run -itd --rm --cap-add=NET_ADMIN --name vpn$i -e BLOCK_MALICIOUS=off -e BLOCK_SURVEILLANCE=off -e BLOCK_ADS=off -e DOT=off -e VPN_SERVICE_PROVIDER=winscribe -e VPN_TYPE=openvpn -e OPENVPN_USER="mgqt21sr-dmvtns9" -e OPENVPN_PASSWORD="uhma4qbxyn" -e SERVER_REGIONS="${array[$index]}" qmcgaw/gluetun
+  until docker top vpn$i; do echo hello && sleep 2; done
+  sleep 10
+  docker run -itd --rm --name gaga$i --network=container:vpn$i --env TOKEN=oaownzgjaiogwiyza71a4d6da519752c jepbura/gaganode
+  docker logs vpn$i && sleep 5;
+  done
+
+apt update && apt install sudo docker.io curl 
