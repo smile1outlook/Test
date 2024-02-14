47a1b8d6e9c1e3e8a1aee7aae4a9df5e9c67a8dd
smile1outlook <153255193+smile1outlook@users.noreply.github.com>
2024-02-15 01:59:42 +0700

    Update gaganodeorg_21.sh

--- gaganodeorg_21.sh
+++ gaganodeorg_21.sh
@@ -4,7 +4,7 @@ apt update && apt install sudo docker.io curl
 
 array=("US West" "US East" "US Central" "South Korea") && size=${#array[@]} && for i in {1..100}; do
   index=$(($RANDOM % $size))
-  docker run -itd --rm --cap-add=NET_ADMIN --name vpn$i -e BLOCK_MALICIOUS=off -e BLOCK_SURVEILLANCE=off -e BLOCK_ADS=off -e DOT=off -e VPN_SERVICE_PROVIDER=windscribe -e VPN_TYPE=openvpn -e OPENVPN_USER="2jaentdo-gy479f" -e OPENVPN_PASSWORD="u65s2k8d9b" -e SERVER_REGIONS="${array[$index]}" qmcgaw/gluetun
+  docker run -itd --rm --cap-add=NET_ADMIN --name vpn$i -e BLOCK_MALICIOUS=off -e BLOCK_SURVEILLANCE=off -e BLOCK_ADS=off -e DOT=off -e VPN_SERVICE_PROVIDER=windscribe -e VPN_TYPE=openvpn -e OPENVPN_USER="2jaentdo-gy479fv" -e OPENVPN_PASSWORD="u65s2k8d9b" -e SERVER_REGIONS="${array[$index]}" qmcgaw/gluetun
   until docker top vpn$i; do echo hello && sleep 2; done
   sleep 10
   docker run -itd --rm --name gaga$i --network=container:vpn$i --env TOKEN=oaownzgjaiogwiyza71a4d6da519752c jepbura/gaganode
