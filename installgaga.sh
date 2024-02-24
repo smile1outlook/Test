73dda79de0445d61a66b8d333e0fd77dc249f9cc
smile1outlook <153255193+smile1outlook@users.noreply.github.com>
2024-02-24 18:25:50 +0700

    Update installgaga.sh

--- installgaga.sh
+++ installgaga.sh
@@ -39,19 +39,6 @@ sudo curl -o $FILENAME -k $DOWNLOADLINK &&
 sudo tar -zxf $FILENAME &&
 sudo rm -f $FILENAME &&
 cd ./apphub-linux* &&
-sudo ./apphub service remove &&
-sudo ./apphub service install
-sudo ./apphub service start
-sudo ./apphub status &&
-sleep 30 && echo ' ____   ___  _   _ _____
-|  _ \ / _ \| \ | | ____|
-| | | | | | |  \| |  _|
-| |_| | |_| | |\  | |___
-|____/ \___/|_| \_|_____|'
-sudo ./apphub status && 
-sudo ./apps/gaganode/gaganode config set --token=ojxqyftmctxeokuq1450466264c9f23f &&
-sudo ./apphub restart &&
-sudo ./apps/gaganode/gaganode log
 fi
 
 /bin/bash
