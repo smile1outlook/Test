9b49dea284d3830227956a9a9e4cbc0c5e1598f0
smile1outlook <153255193+smile1outlook@users.noreply.github.com>
2024-02-24 20:52:58 +0700

    Update newtest.sh

--- newtest.sh
+++ newtest.sh
@@ -11,7 +11,7 @@ docker stop $(docker ps -aq)
 i=1
 head -n 100 socks.txt | while read proxy; do
 	echo $proxy
-	docker run -itd --rm --name tunsmile$i -e LOGLEVEL=$TUN_LOG_PARAM -e PROXY="socks5://$proxy" -e EXTRA_COMMANDS="$EXTRA_COMMANDS" -v '/dev/net/tun:/dev/net/tun' --cap-add=NET_ADMIN -d xjasonlyu/tun2socks:v2.5.2 /bin/sh
+	docker run -itd --rm --name tunsmile$i -e LOGLEVEL=$TUN_LOG_PARAM -e PROXY="socks5://$proxy" -e EXTRA_COMMANDS="$EXTRA_COMMANDS" -v '/dev/net/tun:/dev/net/tun' --cap-add=NET_ADMIN -d xjasonlyu/tun2socks:v2.5.2
 	sleep 1
 	docker run -itd --rm --name gaga$i --network=container:tunsmile$i --env TOKEN=ojxqyftmctxeokuq1450466264c9f23f smile
 	#until docker logs --tail 2 gaga$i | grep 'node started'; do docker logs --tail 2 gaga$i && sleep 2; done
