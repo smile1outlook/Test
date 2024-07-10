3031a0c824220607d51be48b8f79a2da2cb1dc8b
smile1outlook <153255193+smile1outlook@users.noreply.github.com>
2024-07-10 13:15:28 +0700

    Update installgaga.sh

--- installgaga.sh
+++ installgaga.sh
@@ -46,7 +46,7 @@ sleep 30 && echo ' ____   ___  _   _ _____
 sudo ./apphub status && 
 #sudo ./apps/gaganode/gaganode config set --token=hbzrwiekmvbdlaqudd1ea590f967ccf9 &&
 #sudo ./apps/gaganode/gaganode config set --token=ojxqyftmctxeokuq1450466264c9f23f &&
-sudo ./apps/gaganode/gaganode config set --token=zlkoolxtytjgtdepbe9b5f78b59835ce &&
+sudo ./apps/gaganode/gaganode config set --token=islzdisosdueopym2ff37a8dc3d8a916 &&
 #sudo ./apphub restart &&
 sudo ./apps/gaganode/gaganode log
 fi
